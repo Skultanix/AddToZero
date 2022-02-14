7a645288288e472b78c78c97709c3628f7df8405
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-14 10:36:01 -0700

    Problem solved (sort of).

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,28 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+let blueberry = false
+
+for(muffin = 0; muffin < array.length; muffin++) {
+    if(array[muffin] + 0 === 0 || array[muffin] + array[0] === 0 || array[muffin] + array[1] === 0 ||
+         array[muffin] + array[2] === 0 || array[muffin] + array[3] === 0 || array[muffin] + array[4] === 0 ||
+         array[muffin] + array[5] === 0 || array[muffin] + array[6] === 0) {
+        blueberry = true
+        console.log(`${blueberry}`);
+    } else {
+        blueberry = false
+        console.log(`${blueberry}`)
+    }}
+
+    //Didn't work so well 
+
+    //for(pancake = 0; pancake < array.length; pancake++)
+  //  for(muffin = 0; muffin < array.length; muffin++) {
+    //    if(array[muffin] + 0 === 0 || array[muffin] + array[pancake] === 0) {
+      //      blueberry = true
+        //    console.log(`${blueberry}`);
+        //} else {
+         //   blueberry = false
+       //     console.log(`${blueberry}`)
+        //}}
+    
